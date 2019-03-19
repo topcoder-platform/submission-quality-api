25b174af9c42405778c1e0caad771ec32992fe34
Sachin Maheshwari <sachin.maheshwari@topcoder.com>
2019-03-19 15:22:01 +0530

    fixing build issue by adding latest compiled package-lock.json and changes for auth0-proxy server

--- src/common/helper.js
+++ src/common/helper.js
@@ -7,7 +7,7 @@ const _ = require('lodash')
 const request = require('superagent')
 
 const m2mAuth = require('tc-core-library-js').auth.m2m
-const m2m = m2mAuth(_.pick(config, ['AUTH0_URL', 'AUTH0_AUDIENCE', 'TOKEN_CACHE_TIME']))
+const m2m = m2mAuth(_.pick(config, ['AUTH0_URL', 'AUTH0_AUDIENCE', 'AUTH0_PROXY_SERVER_URL']))
 
 /**
  * Wrap async function to standard express function
